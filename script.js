0d1e46442209fd792628018f2b0f1de771788863
Aelkoulali <abdellah.elkoulali@gmx.fr>
2023-10-04 11:58:22 +0200

    Show Person Based On Item

--- script.js
+++ script.js
@@ -58,4 +58,12 @@ const reviews = [
   job.textContent = item.job;
   info.textContent = item.text;
   });
-  
+
+  // show person based on item
+  function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
